6b2a215231b5964cd4a10b1d0cbd0db19f975bc5
elon <22001029@qq.com>
2025-02-21 15:51:49 +0800

    更新代码

---
+++ script/lessonmgmtproxy.js
@@ -0,0 +1,144 @@
+/**
+ * 远程接口调用代理类
+ */
+// var SERVICE_ADDRESS = "http://47.107.248.50:9527";
+var SERVICE_ADDRESS = "http://localhost:9527";
+
+/**
+ * 处理rest请求结果
+ * 
+ * @param {Http请求} xhr 
+ * @param {回调处理函数} handlerFun 
+ * @returns 函数对象
+ */
+function handleResult(xhr, handlerFun){
+    return function() {
+        var result = JSON.parse(xhr.responseText);
+        handlerFun(result);
+    }
+}
+
+/**
+ * 获取课程列表
+ */
+function queryLessonListProxy(handlerFun){
+    var xhr = new XMLHttpRequest();
+    var requestPath = SERVICE_ADDRESS + "/neo-lesson-mgmt/v1/lesson/query-lesson-list";
+    xhr.onload = handleResult(xhr, handlerFun);
+
+    // 查询课程数据同步执行。因学员数据查询依赖于选择的课程。
+    xhr.open("GET", requestPath, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('account', 'neo');
+
+    xhr.send();
+}
+
+/**
+ * 分页查询学生信息列表
+ * 
+ * @param {回调函数} handlerFun 
+ * @param {分页条件} pageVo 
+ * @param {课程编码} lessonCode 
+ */
+function queryStudentByPageProxy(handlerFun, pageVo, lessonCode) {
+    var xhr = new XMLHttpRequest();
+    var requestPath = SERVICE_ADDRESS + "/neo-lesson-mgmt/v1/student/query-student-by-page/" + lessonCode;
+    xhr.onload = handleResult(xhr, handlerFun);
+
+    xhr.open("POST", requestPath, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('account', 'neo');
+
+    const pageVoJson = JSON.stringify(pageVo);
+    xhr.send(pageVoJson);
+}
+
+/**
+ * 删除课程数据代理接口
+ * 
+ * @param {课程编码} lessonCode 
+ */
+function deleteLessonProxy(lessonCode){
+    var xhr = new XMLHttpRequest();
+    var requestPath = SERVICE_ADDRESS + "/neo-lesson-mgmt/v1/lesson/delete-lesson/" + lessonCode;
+
+    xhr.open("DELETE", requestPath, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('account', 'neo');
+
+    xhr.send();
+}
+
+/**
+ * 插入学员数据
+ * 
+ * @param {学员数据} student
+ */
+function insertStudentProxy(student){
+    var xhr = new XMLHttpRequest();
+    var requestPath = SERVICE_ADDRESS + "/neo-lesson-mgmt/v1/student/insert-student";
+
+    xhr.open("POST", requestPath, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('account', 'neo');
+
+    const studentJson = JSON.stringify(student);
+    xhr.send(studentJson);
+}
+
+/**
+ * 删除学员数据代理接口
+ * 
+ * @param {课程编码} lessonCode 
+ * @param {学员编码} studentCode 
+ */
+function deleteStudentProxy(lessonCode, studentCode){
+    var xhr = new XMLHttpRequest();
+    var requestPath = SERVICE_ADDRESS + "/neo-lesson-mgmt/v1/student/delete-student/" + lessonCode + "/" + studentCode;
+
+    xhr.open("DELETE", requestPath, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('account', 'neo');
+
+    xhr.send();
+}
+
+/**
+ * 增加课时数量代理接口
+ * 
+ * @param {课程编码} lessonCode 
+ * @param {学员编码} studentCode 
+ * @param {课时数量} lessonNum 
+ */
+function increaseLessonNumProxy(lessonCode, studentCode, lessonNum){
+    var xhr = new XMLHttpRequest();
+    var requestPath = SERVICE_ADDRESS + "/neo-lesson-mgmt/v1/student/increase-lesson-num/" + lessonCode 
+                    + "/" + studentCode + "/" + lessonNum;
+
+    xhr.open("POST", requestPath, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('account', 'neo');
+
+    xhr.send();
+}
+
+/**
+ * 扣减课时数量代理接口
+ * 
+ * @param {课程编码} lessonCode 
+ * @param {学员编码} studentCode 
+ * @param {课时数量} lessonNum 
+ */
+function decreaseLessonNumProxy(lessonCode, studentCode, lessonNum){
+    var xhr = new XMLHttpRequest();
+    var requestPath = SERVICE_ADDRESS + "/neo-lesson-mgmt/v1/student/decrease-lesson-num/" + lessonCode 
+                    + "/" + studentCode + "/" + lessonNum;
+
+    xhr.open("POST", requestPath, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('account', 'neo');
+
+    xhr.send();
+}
+
---
+++ script/studentcontent.js
@@ -0,0 +1,176 @@
+
+// 记录当前选中的课程编码
+var CURRENT_LESSON_CODE;
+
+// 记录当前操作的学员编码
+var CURRENT_STUDENT_CODE;
+
+/**
+ * 加载选中课程包含的学员
+ */
+function loadStudentOfSelectedLesson(){
+    // 1、获取选中的课程编码
+    const lessonList = document.getElementsByClassName("selected-lesson");
+    if (lessonList.length == 0) {
+        return;
+    }
+
+    // DOM的id保存的是课程编码
+    const lessonCode = lessonList[0].id;
+    CURRENT_LESSON_CODE = lessonCode;
+    
+    // 2、查询课程包含的学员更新表格
+    var pageVo = {
+        "amount": 10,
+        "pageNo": 1
+      }
+    
+    queryStudentByPageProxy(initStudentTable, pageVo, lessonCode);
+}
+
+
+/**
+ * 初始化学员列表
+ * 
+ * @param {查询结果} result 
+ */
+function initStudentTable(result){
+    var studentHtml = "";
+    studentHtml += "<tr> "
+                    + "<th class='operator'>操作</th>"
+                    + "<th class='name' scope='col'>姓名</th>"
+                    + "<th class='lesson'>课程</th>"    
+                    + "<th class='lesson-num' scope='col'>剩余课时</th>"  
+                    + "<th class='email' scope='col'>email</th>"   
+                + "</tr>"
+    var studentList = result.data.dataList;
+    for(var i = 0; i < studentList.length; ++i){
+        const student = studentList[i];
+        studentHtml += '<tr id="' + student.studentCode +'">'
+                        + '<td class="operator clearfix">'
+                        + '<button class="operator-btn float-left" onclick="deleteStudent(this)">删除</button>'
+                        +' <button class="operator-btn float-left" onclick="increaseLessonNum(this)">加课时</button>'
+                        +' <button class="float-left" onclick="decreaseLessonNum(this)">减课时</button></td>'
+                        + '<td class="name">' + student.name + '</td>'
+                        + '<td class="lesson">' + student.lessonName +'</td>'
+                        + '<td class="lesson-num">' + student.surplusLessonNum + '</td>'
+                        + '<td class="email">' + student.email +'</td>'
+                    + '</tr>';
+    }
+
+    document.getElementById("student-table-id").innerHTML = studentHtml;
+}
+
+/**
+ * 显示添加学员对话框
+ */
+function showAddStudentDialog(){
+    var dialog = document.getElementById("add-student-dialog-id");
+    dialog.classList.remove("hidden");
+}
+
+/**
+ * 取消添加学员
+ */
+function cancelAddStudent(){
+    document.getElementById("add-student-dialog-id").classList.add("hidden");
+}
+
+/**
+ * 确定添加学员
+ */
+function confirmAddStudent(){
+    var name =  document.getElementById("add-student-dialog-name-id").value;
+    var surplusLessonNum = document.getElementById("add-student-dialog-surplus-lesson-num-id").value;
+    var email = document.getElementById("add-student-dialog-email-id").value;
+
+    var student = {
+        "lessonCode": CURRENT_LESSON_CODE,
+        "name": name,
+        "surplusLessonNum": surplusLessonNum,
+        "email": email
+    }
+
+    insertStudentProxy(student);
+    loadStudentOfSelectedLesson();
+
+    // 关闭对话框
+    document.getElementById("add-student-dialog-id").classList.add("hidden");
+}
+
+/**
+ * 删除学员
+ * 
+ * @param {删除按钮} item 
+ */
+function deleteStudent(item){
+    var studentRow = item.parentNode.parentNode;
+    const studentCode = studentRow.id;
+
+    deleteStudentProxy(CURRENT_LESSON_CODE, studentCode);
+    loadStudentOfSelectedLesson();
+}
+
+/**
+ * 弹出加课时对话框
+ * 
+ * @param {加课时按钮对象} item 
+ */
+function increaseLessonNum(item){
+    var studentRow = item.parentNode.parentNode;
+    CURRENT_STUDENT_CODE = studentRow.id;
+
+    var dialog = document.getElementById("increase-less-num-dialog-id");
+    dialog.classList.remove("hidden");
+}
+
+/**
+ * 取消增加课时
+ */
+function cancelIncreaseLessNum(){
+    document.getElementById("increase-less-num-dialog-id").classList.add("hidden");
+}
+
+/**
+ * 确认增加课时
+ */
+function confirmIncreaseLessNum(){
+    var incrLessonNum = document.getElementById("increase-less-num-dialog-lesson-num-id").value;
+
+    increaseLessonNumProxy(CURRENT_LESSON_CODE, CURRENT_STUDENT_CODE, incrLessonNum);
+    loadStudentOfSelectedLesson();
+
+    document.getElementById("increase-less-num-dialog-id").classList.add("hidden");
+}
+
+/**
+ * 弹出扣减课时对话框
+ * 
+ * @param {减课时按钮} item 
+ */
+function decreaseLessonNum(item){
+    var studentRow = item.parentNode.parentNode;
+    CURRENT_STUDENT_CODE = studentRow.id;
+
+    var dialog = document.getElementById("decrease-less-num-dialog-id");
+    dialog.classList.remove("hidden");
+}
+
+/**
+ * 取消减课时
+ */
+function cancelDecreaseLessNum(){
+    document.getElementById("decrease-less-num-dialog-id").classList.add("hidden");
+}
+
+/**
+ * 确认扣减课时
+ */
+function confirmDecreaseLessNum(){
+    var incrLessonNum = document.getElementById("decrease-less-num-dialog-lesson-num-id").value;
+
+    decreaseLessonNumProxy(CURRENT_LESSON_CODE, CURRENT_STUDENT_CODE, incrLessonNum);
+    loadStudentOfSelectedLesson();
+
+    document.getElementById("decrease-less-num-dialog-id").classList.add("hidden");
+}
